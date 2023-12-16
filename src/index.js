93723343f98b34dc488666946075ec213686e3bc
abubkar <abubakaraw8@gmail.com>
2023-12-16 17:32:15 +0500

    configure cookieparser cors and more

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { express } from "express";
+
+const app = express();
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log("Server started on port", process.env.PORT);
+    })
+})
+.catch((err) => {
+    console.log("Mongodb connection failed !!! ", err);
+})
